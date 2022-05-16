f4c9468f4becb8576a4a1c88413a4dfa73785a60
Prince Codes <princecodes247@gmail.com>
2022-05-16 07:30:37 -0400

    update arbitrage contract

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -11,18 +11,19 @@ contract Arbitrage {
 
     constructor(address DEX1, address DEX2) {}
 
-    // Swap token A for token B
-    function performArbitrage(uint _amount) public {
-        require(
-            tokenA.balanceOf(msg.sender) >= _amount,
-            "You need more Token A"
-        );
-        require(_amount > 0, "You need to use at least some tokens");
+    // Fund Contract with Token A
+    function fundContract(uint256 _amount) public {
+        require(tokenA.balanceOf(msg.sender) >= _amount, "You need more Token A");
+        require(_amount > 0, "You need to sell at least some tokens");
+        // require(_amount <= maxSwapLimit, "You've exceeded the max swap limit!");
         uint256 approvedAmt = tokenA.allowance(msg.sender, address(this));
         require(approvedAmt >= _amount, "Check the token allowance");
 
         tokenA.transferFrom(msg.sender, address(this), _amount);
+    }
 
-
+    // Swap token A for token B
+    function performArbitrage(uint256 _amount) public {
+        
     }
 }
